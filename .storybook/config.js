59a8a25c876c7824459e831d242ebf0031cf5c2e
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-06-12 01:23:13 +0900

    chore: modify context path

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure, addDecorator } from '@storybook/angular';
 import { initScreenshot, withScreenshot } from 'storybook-chrome-screenshot';
 
 // automatically import all files ending in *.stories.ts
-const req = require.context('../src/stories', true, /.stories.ts$/);
+const req = require.context('../src', true, /.stories.ts$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
