c256d46f892689dfe41aefb6955f74e70f366232
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-06-16 05:00:00 +0900

    modify

--- src/app/hero-unit/hero-unit.component.ts
+++ src/app/hero-unit/hero-unit.component.ts
@@ -3,16 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero-unit',
   template: `
-  <div>
+  <div class="inner">
     <img src="/assets/logo.png" />
-    <p>{{greeting}}</p>
+    <p class="message">{{greeting}}</p>
   </div>
   `,
   styleUrls: ['./hero-unit.component.css']
 })
 export class HeroUnitComponent implements OnInit {
 
-  greeting: string = 'Hello, ng-japan!';
+  greeting: string = 'Hello, ng-japan 2018';
 
   constructor() { }
 
